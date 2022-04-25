024e99899a7a39c48652cb182975c9ce8dbf0eab
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-25 22:30:40 +0530

    Update receiver_test.py

--- receiver_test.py
+++ receiver_test.py
@@ -3,10 +3,8 @@ import unittest
 import receiver_test_input
 
 class receiver_test(unittest.TestCase):
-  
-  def test(self):
-    self.assertTrue((receiver.update_extremes(receiver_test_input.meta_data_sample1, 25) == {'minimum_value': 10, 'maximum_value': 25, 'moving_avg': 0, 'moving_window': 5, 'index_of_next_item': -5})
-        
-                    
-                    
-unittest.main()
+    def test(self):
+        self.assertTrue(receiver.update_extremes(receiver_test_input.meta_data_sample1, 25) == {'minimum_value': 10, 'maximum_value': 25, 'moving_avg': 0, 'moving_window': 5, 'index_of_next_item': -5})
+
+
+unittest.main(),
