993437d467c9cd6cf465a1e9ad270e7218e8e5dd
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-25 22:12:08 +0530

    Update receiver_test_input.py

--- receiver_test_input.py
+++ receiver_test_input.py
@@ -1,1 +1,22 @@
-
+# metadata
+meta_data_sample1 = {
+                    "minimum_value"     : 10,
+                    "maximum_value"     : 5 ,
+                    "moving_avg"        : 0,
+                    "moving_window"     : 5,
+                    "index_of_next_item": -5,
+                    }
+meta_data_sample2 = {
+                    "minimum_value"     : 5,
+                    "maximum_value"     : 10,
+                    "moving_avg"        : 0,
+                    "moving_window"     : 5,
+                    "index_of_next_item": -5,
+                    }
+meta_data_sample2 = {
+                    "minimum_value"     : 0,
+                    "maximum_value"     : 0,
+                    "moving_avg"        : 0,
+                    "moving_window"     : 5,
+                    "index_of_next_item": -5,
+                    }
