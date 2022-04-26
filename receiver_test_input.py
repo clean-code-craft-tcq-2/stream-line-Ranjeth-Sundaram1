191442b082ec0cad179900a49295e24d4bc0cdb1
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-26 22:11:35 +0530

    Update receiver_test_input.py

--- receiver_test_input.py
+++ receiver_test_input.py
@@ -21,5 +21,6 @@ update_extremes_expt_outcome_test2 = {'minimum_value': 0, 'maximum_value': 5, 'm
 update_moving_average_expt_outcome_test1 = {'minimum_value': 10, 'maximum_value': 5, 'moving_avg': 0, 'moving_window': [2, 0, 0, 0, 0], 'index_of_next_item': -4}
 update_moving_average_expt_outcome_test2 = {'minimum_value': 10, 'maximum_value': 5, 'moving_avg': 1.0, 'moving_window': [0, 0, 0, 5, 0], 'index_of_next_item': 4}
 
-setup_expt_outcome_test1 = [{'minimum_value': None, 'maximum_value': None, 'moving_avg': None, 'moving_window': [None, None], 'index_of_next_item': -2}, {'minimum_value': None, 'maximum_value': None, 'moving_avg': None, 'moving_window': [None, None], 'index_of_next_item': -2}]
+# setup_expt_outcome_test1 = [{'minimum_value': None, 'maximum_value': None, 'moving_avg': None, 'moving_window': [None, None], 'index_of_next_item': -2}, {'minimum_value': None, 'maximum_value': None, 'moving_avg': None, 'moving_window': [None, None], 'index_of_next_item': -2}]
 
+setup_expt_outcome_test1 =[{'minimum_value': None, 'maximum_value': None, 'moving_avg': None, 'moving_window': [None, None, None, None, None], 'index_of_next_item': -5}, {'minimum_value': None, 'maximum_value': None, 'moving_avg': None, 'moving_window': [None, None, None, None, None], 'index_of_next_item': -5}]
