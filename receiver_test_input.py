ebc1aa944ee382f6c92d1d1dfe53c409b2a56319
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-26 21:54:19 +0530

    Update receiver_test_input.py

--- receiver_test_input.py
+++ receiver_test_input.py
@@ -18,5 +18,5 @@ meta_data_sample2 = {
 update_extremes_expt_outcome_test1 = {'minimum_value': 10, 'maximum_value': 25, 'moving_avg': 0, 'moving_window': [0,0,0,0,0], 'index_of_next_item': -5}
 update_extremes_expt_outcome_test2 = {'minimum_value': 0, 'maximum_value': 5, 'moving_avg': 0, 'moving_window': [0,0,0,0,0], 'index_of_next_item': -5}
 
-update_moving_average_expt_outcome_test1 = "{'minimum_value': 10, 'maximum_value': 5, 'moving_avg': 0, 'moving_window': [2, 0, 0, 0, 0], 'index_of_next_item': -4}"
+update_moving_average_expt_outcome_test1 = {'minimum_value': 10, 'maximum_value': 5, 'moving_avg': 0, 'moving_window': [2, 0, 0, 0, 0], 'index_of_next_item': -4}
 update_moving_average_expt_outcome_test2 = {'minimum_value': 10, 'maximum_value': 5, 'moving_avg': 1.0, 'moving_window': [0, 0, 0, 5, 0], 'index_of_next_item': 4}
