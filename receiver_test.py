eb5c905e20f137ae0785f1b006f3e56e42e62be7
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-04-26 21:50:07 +0530

    Update receiver_test.py
    Update update_extremes and update_moving_average tests

--- receiver_test.py
+++ receiver_test.py
@@ -4,11 +4,15 @@ import receiver_test_input
 
 class receiver_test(unittest.TestCase):
     def test_update_extremes(self):
-        self.assertTrue(receiver.update_extremes(receiver_test_input.meta_data_sample1.copy, 25) == {'minimum_value': 10, 'maximum_value': 25, 'moving_avg': 0, 'moving_window': [0,0,0,0,0], 'index_of_next_item': -5})
-        self.assertTrue(receiver.update_extremes(receiver_test_input.meta_data_sample1.copy, 0) == {'minimum_value': 0, 'maximum_value': 5, 'moving_avg': 0, 'moving_window': [0,0,0,0,0], 'index_of_next_item': -5})
+        self.assertTrue(receiver.update_extremes(receiver_test_input.meta_data_sample1.copy(), 25) == update_extremes_expt_outcome_test1)
+        self.assertTrue(receiver.update_extremes(receiver_test_input.meta_data_sample1.copy(), 0) == update_extremes_expt_outcome_test2)
     
-    def test_pick_sample_from_console(self):
-        self.assertTrue(receiver.pick_sample_from_console([]) == "")
-        self.assertTrue(receiver.pick_sample_from_console(["{'Current': 8, 'Temp': 39}"]) == "Current\t: Min = 8\tMax = 8\tAvg = None\nTemp\t: Min = 39\tMax = 39\tAvg = None\n")
+    def test_update_moving_average(self):
+        self.assertTrue(receiver.update_moving_average(meta_data_sample1.copy(), 2) == update_moving_average_expt_outcome_test1)
+        self.assertTrue(receiver.update_moving_average(meta_data_sample2.copy(), 5) == update_moving_average_expt_outcome_test2)
+
+#     def test_pick_sample_from_console(self):
+#         self.assertTrue(receiver.pick_sample_from_console([]) == "")
+#         self.assertTrue(receiver.pick_sample_from_console(["{'Current': 8, 'Temp': 39}"]) == "Current\t: Min = 8\tMax = 8\tAvg = None\nTemp\t: Min = 39\tMax = 39\tAvg = None\n")
 
 unittest.main(),
